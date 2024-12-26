f7b120cfa354b231eccd4c8e0f7857e2ecb14118
Anezijane <ahnnieanezi@gmail.com>
2024-12-26 15:17:41 +0100

    add navbar

---
+++ src/Component/FreeTips.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FreeTips = () => {
+  return (
+    <div>
+      freetips
+    </div>
+  )
+}
+
+export default FreeTips
---
+++ src/Component/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className='pt-20'>
+        <header  className='h-screen'>Landing</header>
+        <section className='h-screen'>Below</section>
+        <section className='h-screen'>Below</section>
+    </main>
+  )
+}
+
+export default Home

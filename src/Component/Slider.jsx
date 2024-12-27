c4073e29b8938b37608acff3ee48f6ae50742c33
Anezijane <ahnnieanezi@gmail.com>
2024-12-27 21:45:39 +0100

    update home page

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,7 +1,7 @@
 import { useState} from "react";
 import h1Hero from "../assets/img/hero/h1_hero.png";
-import gallery4 from "../assets/img/gallery/gallery4.png";
-import gallery1 from "../assets/img/gallery/gallery1.png";
+import gallery4 from "../assets/img/gallery/herobg2.jpeg";
+import gallery1 from "../assets/img/gallery/gallery4.png";
 
 const Slider = () => {
   const slides = [
@@ -26,7 +26,10 @@ const Slider = () => {
 
   return (
     <div className="max-w-[1400px] h-[450px] w-full m-auto relative">
-        <div style={{backgroundImage: `url(${slides[0].background})`}} className="w-full h-full bg-center bg-cover duration-500"></div>
+      <div style={{backgroundImage: `url(${slides[1].background})`}} className="w-full h-full bg-center bg-cover duration-500">
+
+        <div className="absolute inset-0 bg-black/70"></div>
+      </div>
     </div>
   );
 };

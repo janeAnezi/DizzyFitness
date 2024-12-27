888c99c633cf88a44277157055cd809b5ffc6d88
Anezijane <ahnnieanezi@gmail.com>
2024-12-27 23:35:18 +0100

    update home page

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -30,13 +30,13 @@ const Slider = () => {
   }
   const nextSlide = () => {
     const lastSlide = currIndex === slides.length - 1;
-    setCurrIndex(lastSlide ? 0 : currIndex - 1);
+    setCurrIndex(lastSlide ? 0 : currIndex + 1);
   }
 
   return (
     <div className="max-w-[1400px] h-[450px] w-full m-auto relative group">
       <div style={{backgroundImage: `url(${slides[currIndex].background})`}} 
-        className="w-full h-full bg-center bg-cover  duration-500">
+        className="w-full h-full bg-center bg-cover duration-500">
         <div className="absolute inset-0 bg-black/70"></div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-gray-50">
             <h1 className="text-2xl">{slides[currIndex].title}</h1>
@@ -44,11 +44,11 @@ const Slider = () => {
         </div>
       </div>
         <div  
-          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl text-gray-400 cursor-pointer">
+          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl text-gray-400 cursor-pointer z-10">
           < IoIosArrowBack size={30} onClick={prevSlide}  />
         </div>
         <div 
-          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl text-gray-400 cursor-pointer">
+          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl text-gray-400 cursor-pointer z-10">
             <IoIosArrowForward size={30} onClick={nextSlide}/>
         </div>
     </div>
